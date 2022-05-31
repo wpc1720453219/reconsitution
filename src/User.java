bb55c3d8931e54614067ea794992dd14b5d6ce63
王鹏程 <1720453219@qq.com>
2022-05-31 23:17:59 +0800

    第6章

---
+++ src/User.java
@@ -0,0 +1,38 @@
+public class User {
+    private String name;
+    private String address;
+    private String mail;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
